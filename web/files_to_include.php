b0b23dfe563241f330f571303a86f5d860a997d5
Bredge <quentin.aubert1226@gmail.com>
2021-09-15 15:11:12 +0200

    let's go

---
+++ web/files_to_include.php
@@ -0,0 +1,8 @@
+<?php
+//require_once '../app/Models/get_db_parameters.php';
+//require_once '../app/Models/BDD_connect.php';
+//require_once '../app/Models/LivreModel.php';
+//require_once '../app/Controllers/LivreController.php';
+//require_once '../app/util/View.php';
+//require_once '../app/util/Route.php';
+?>
---
+++ web/index.php
@@ -0,0 +1,24 @@
+﻿﻿<?php // index.php
+session_start();
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+require_once 'files_to_include.php';
+
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+use Util\Route;
+
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+$_SESSION['Index_path'] = explode('/', trim($_SERVER['SCRIPT_NAME'],'/'));
+$_SESSION['Index_path_length'] = count($_SESSION['Index_path']);
+
+// /*Code test*/
+// echo '<pre>';
+// print_r($_SESSION['Index_path']);
+// echo '</pre><br><br>';
+// echo 'Longueur du tableau :'.$_SESSION['Index_path_length'].'<br><br>';
+
+$routeur = new Route;
+
+?>
