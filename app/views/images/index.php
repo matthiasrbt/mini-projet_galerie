2dd718dad7abe83c26c586449483f1b2f3adb907
Matthias Robert <matthias.rbt120@gmail.com>
2021-09-22 17:59:37 +0200

    Ajout card template

--- app/views/images/index.php
+++ app/views/images/index.php
@@ -20,8 +20,29 @@ echo <<<_END
                 </div>
               </div>
         </nav>
-        <p>HelloWord</p>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul class="list-group">
+                        <div class="card mb-3" style="max-width: 540px;">
+                          <div class="row g-0">
+                            <div class="col-md-4">
+                              <img src="..." class="img-fluid rounded-start" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                              <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </ul>
+                </div>
+                <div class="col"></div>
+            </div>
+        </div> 
         <p><a href="../app/download.php?path=../tmp/JFLAP 01 Starting up and first DFA.mp4">Download mp4 file</a></p>
         <p><a href="../app/download.php?path=../tmp/oui.txt">Download TEXT file</a></p>
     </body>   
