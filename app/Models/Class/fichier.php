f424ce92ac9ea5a5347cf2a67f441fbbaab4197e
AlexandreBlcht <blanchet_a0904@outlook.fr>
2021-09-22 10:31:12 +0200

    class "fichier"

---
+++ app/Models/Class/fichier.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace model;
+
+
+class fichier
+{
+    var $chemin ;
+    var $nom;
+    var $type;
+
+    /**
+     * fichier constructor.
+     * @param $chemin
+     * @param $nom
+     * @param $type
+     */
+    public function __construct($chemin, $nom, $type)
+    {
+        $this->chemin = $chemin;
+        $this->nom = $nom;
+        $this->type = $type;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getChemin()
+    {
+        return $this->chemin;
+    }
+
+    /**
+     * @param mixed $chemin
+     */
+    public function setChemin($chemin): void
+    {
+        $this->chemin = $chemin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type): void
+    {
+        $this->type = $type;
+    }
+
+
+
+}
