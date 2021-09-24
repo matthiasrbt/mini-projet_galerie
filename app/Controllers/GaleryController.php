b22bb42356dc8c21ae8a83b87da4218eabdf3396
Bredge <quentin.aubert1226@gmail.com>
2021-09-24 19:15:47 +0200

    transi upload

--- app/Controllers/GaleryController.php
+++ app/Controllers/GaleryController.php
@@ -1,5 +1,6 @@
 <?php namespace Controllers;
 
+use Imagick;
 use util\View;
 use Models\containerFichier;
 use Models\fichier;
@@ -13,7 +14,29 @@ class GaleryController
             $_SESSION['listeFichiers'] = new containerFichier();
         }
         $listeFichier = $_SESSION['listeFichiers']->listeLesFichiersParType();
+        /*foreach($listeFichier as $unFichier)
+        {
+            echo 'Route : '.$unFichier['chemin'].'<br>';
+            if($unFichier['type'] == "pdf")
+            {
+                $im = new Imagick('../tmp/'.$unFichier['nom'].'.pdf');
+                $im->setIteratorIndex(0);
+                $im->setCompression(Imagick::COMPRESSION_LZW);
+                $im->setCompressionQuality(90);
+                $im->writeImage('../tmp/miniatures/'.$unFichier['nom'].'.png');
+                $unFichier['miniature'] = '../tmp/miniatures/'.$unFichier['nom'].'.png';
+                echo 'Route miniature pdf: '.$unFichier['chemin'].'<br><br>';
+
+                //<embed width="191" height="207" name="plugin" src="http://localhost:54149/Documento/VersaoView?chave=FDC4875EE17FB17B" type="application/pdf">
+            }
+        }*/
         $view = new View();
         $view->view('card_galerie.twig', ['files' => $listeFichier ]);
+    }
+
+    public function uploadFile()
+    {
+        $view = new View();
+        $view->view('upload.twig', '');
     }
 }
